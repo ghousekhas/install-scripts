e0f4713c7ce5fba8927c05c77081a3f64ed4bcce
ghousekhas <ghouse.khasim1@gmail.com>
2021-05-29 15:56:35 +0530

    configs and install scripts for kubuntu debian

---
+++ first_install.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+echo groot | sudo apt-get update
+sudo apt upgrade -y
+sudo apt-get install snap git build-essential g++ gcc dpkg openjdk-17-jdk qbittorrent vlc -y
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo snap install snap-store
+sudo snap install pycharm-community --classic
+sudo snap install code-insiders --classic
+sudo snap install android-studio --classic
+sudo snap install node --classic
+sudo snap install postman libreoffice whatsie telegram-desktop jupyter zoom-client teams-for-linux
+sudo snap install sublime-text gitkraken node android-studio code-insiders pycharm-community rider idea openjdk --classic
+npm install -g react-native-cli expo-cli @angular/cli
+sudo snap install flutter --classic
+sudo apt-get install python3 python ns2 -y
+sudo apt install apache2 -y
+sudo apt install mariadb-server mariadb-client -y
+sudo apt install htop php7.4 php7.4-mysql php-common php7.4-cli php7.4-json php7.4-common php7.4-opcache libapache2-mod-php7.4 -y
